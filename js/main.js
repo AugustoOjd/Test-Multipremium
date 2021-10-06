231894fb6bc5503173d3d415c9328b18b05d9914
Augusto Ojeda <okarinex@gmail.com>
2021-10-05 21:01:43 -0300

    agregando header, edicion limitadaa e imaganes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
 
-let edad = prompt("Cuantos años tienes?")
-let nombre= prompt("Como te llamas?")
-
-if(edad >= 18){
-    alert(`${nombre} eres mayor de edad, puedes ingresar`)
-}
-
-else if((edad == "") || (nombre == "")){
-    alert("Te faltaron datos")
-}
-
-else{
-    alert(`${nombre} eres menor de edad, no puedes ingresar. Cuidate`)
-}
-
-
 
