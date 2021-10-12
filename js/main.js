db82935db33b6fc7d7d6511455ae6dc831e7559e
Augusto Ojeda <okarinex@gmail.com>
2021-10-12 20:06:59 -0300

    agregando ciclo para el desafio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
+// Ciclo
+
+for (let i=0; i <=15; i++){
+
+    if(i == 5){
+        continue;
+    }
+    else if(i ==10){
+        continue;
+    }
+    else if(i ==15){
+        
+    }
+
+
+    console.log(i);
+}
 
